12cb02c9b6852582db60aeecd6a28c67ccad44ba
s-liku <s-liku@53S500038274.it.bsd405.org>
2019-09-20 13:41:17 -0700

    Ascii art

--- ASCIIArt/src/ASCIIArt.java
+++ ASCIIArt/src/ASCIIArt.java
@@ -17,11 +17,73 @@
 // YOU CODE HERE :) 
 
 public class ASCIIArt {
+	public static void Congras() {
+		System.out.println("  _____                            _         _       _   _                     ____                    _   _   _ ");
+		System.out.println(" / ____|                          | |       | |     | | (_)                   |  _ \\                  | | | | (_)");
+		System.out.println("| |     ___  _ __   __ _ _ __ __ _| |_ _   _| | __ _| |_ _  ___  _ __  ___    | |_) |_   _  __ _  __ _| |_| |_ _ ");
+		System.out.println("| |    / _ \\| \'_ \\ / _` | \'__/ _` | __| | | | |/ _` | __| |/ _ \\| \'_ \\/ __|   |  _ <| | | |/ _` |/ _` | __| __| |");
+		System.out.println("| |___| (_) | | | | (_| | | | (_| | |_| |_| | | (_| | |_| | (_) | | | \\__ \\   | |_) | |_| | (_| | (_| | |_| |_| |");
+		System.out.println(" \\_____\\___/|_| |_|\\__, |_|  \\__,_|\\__|\\__,_|_|\\__,_|\\__|_|\\___/|_| |_|___/   |____/ \\__,_|\\__, |\\__,_|\\__|\\__|_|");
+		System.out.println("                    __/ |                                                                   __/ |                ");
+		System.out.println("                   |___/                                                                   |___/                 ");
+	}
+	public static void reaching() {
+		System.out.println("   ______                               _     _              ");
+		System.out.println("  |  ____|                             | |   (_)            ");
+		System.out.println("  | |__ ___  _ __   _ __ ___  __ _  ___| |__  _ _ __   __ _ ");
+		System.out.println("  |  __/ _ \\| \'__| | \'__/ _ \\/ _` |/ __| \'_ \\| | \'_ \\ / _` |");
+		System.out.println("  | | | (_) | |    | | |  __/ (_| | (__| | | | | | | | (_| |");
+		System.out.println("  |_|  \\___/|_|    |_|  \\___|\\__,_|\\___|_| |_|_|_| |_|\\__, |");
+		System.out.println("                                                       __/ |");
+		System.out.println("                                                      |___/ ");
+	}
+	public static void mph() {
+		System.out.println("	  ________  ________  ________          _____ ______   ________  ___  ___          ___  ___  ___       ");
+		System.out.println("	 |\\_____  \\|\\   __  \\|\\   __  \\        |\\   _ \\  _   \\|\\   __  \\|\\  \\|\\  \\        |\\  \\|\\  \\|\\  \\      ");
+		System.out.println("	 \\|____|\\  \\ \\  \\|\\  \\ \\  \\|\\  \\       \\ \\  \\\\\\__\\ \\  \\ \\  \\|\\  \\ \\  \\\\\\  \\       \\ \\  \\ \\  \\ \\  \\     ");
+		System.out.println("	       \\|\\  \\ \\  \\\\\\  \\ \\  \\\\\\  \\       \\ \\  \\\\|__| \\  \\ \\   ____\\ \\   __  \\       \\ \\  \\ \\  \\ \\  \\    ");
+		System.out.println("	      __\\_\\  \\ \\  \\\\\\  \\ \\  \\\\\\  \\       \\ \\  \\    \\ \\  \\ \\  \\___|\\ \\  \\ \\  \\       \\ \\__\\ \\__\\ \\__\\   ");
+		System.out.println("	     |\\_______\\ \\_______\\ \\_______\\       \\ \\__\\    \\ \\__\\ \\__\\    \\ \\__\\ \\__\\       \\|__|\\|__|\\|__|   ");
+		System.out.println("	     \\|_______|\\|_______|\\|_______|        \\|__|     \\|__|\\|__|     \\|__|\\|__|           ___  ___  ___ ");
+		System.out.println("	                                                                                        |\\__\\|\\__\\|\\__\\");
+		System.out.println("	                                                                                        \\|__|\\|__|\\|__|");
+	}
+	public static void car() {
+		System.out.println("                 _____________-_____________                 ");
+		System.out.println("              /                               \\              ");
+		System.out.println("          /                                       \\          ");
+		System.out.println("    /-| /____________________---____________________\\ |-\\  ");
+		System.out.println("   \\___\\__                   \\|/                   __/___/  ");
+		System.out.println("      /   \\_______            |           ________/   \\      ");
+		System.out.println("     /            \\__________ | _________/             \\     ");
+		System.out.println("    |-________________       \\|/         ______________-|    ");
+		System.out.println("    |  |__|__|__|__|__     --- ---     __|__|__|__|__|  |    ");
+		System.out.println("    |                    --- ___ ---                    |    ");
+		System.out.println("    |   /----------\\     --- --- ---     /----------\\   |    ");
+		System.out.println("    |   \\-----------\\    ---     ---    /-----------/   |    ");
+		System.out.println("    |    \\----------/    ---     ---    \\----------/    |    ");
+		System.out.println("    |\\ __________________---_____---__________________ /|    ");
+		System.out.println("    |_-|_-|_-|_-|                           |-_|-_|-_|-_|    ");
+	}
+		
+		
+		
+	
+	
 	public static void main (String [] args) {
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
-		System.out.println("THIS IS MY ASCII ART!!!!");
+//		System.out.println("THIS IS MY ASCII ART!!!!");
+//		System.out.println("THIS IS MY ASCII ART!!!!");
+//		System.out.println("THIS IS MY ASCII ART!!!!");
+//		System.out.println("THIS IS MY ASCII ART!!!!");
+//		System.out.println("THIS IS MY ASCII ART!!!!");
+		Congras();
+		System.out.println("\n");
+		reaching();
+		System.out.println("\n");
+		mph();
+		System.out.println("\n");
+		car();
+	
+		
 	}
 }
